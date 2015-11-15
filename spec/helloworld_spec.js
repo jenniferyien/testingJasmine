76a050b1eca120a69067e7434cc8a68d9c33b0f4
Jennifer Yien <jenniferllwu@gmail.com>
2015-11-15 11:01:39 -0500

    testing

--- spec/helloworld_spec.js
+++ spec/helloworld_spec.js
@@ -1,5 +1,11 @@
 describe("Hello World", function(){
   it("should return hello world", function(){
-    expect(helloWorld()).toEqual("Hello World"); 
+    expect(helloWorld()).toEqual("Hello World");
   })
 });
+
+describe("name", function(){
+  it("name of the site", function(){
+    expect(name()).toEqual("What is my name?");
+  })
+})
--- src/helloWorld.js
+++ src/helloWorld.js
@@ -1,3 +1,7 @@
 function helloWorld(){
   return "Hello World";
 }
+
+function name(){
+  return "What is my name?";
+}
